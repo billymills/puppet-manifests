50cc8ac549ed7e3f11596d0c4f2c6fa071056d94
root <root@learn.localdomain>
2013-03-21 12:56:41 +0000

    working with mysql module successfully creating users

--- mysql_module_test.pp
+++ mysql_module_test.pp
@@ -1,2 +1,44 @@
-class { 'mysql::server': }
-class { 'mysql': }
+$mysql_root_pw = 'puppet'
+
+class { 'mysql::server': 
+  config_hash       => {
+      root_password => 'puppet',
+  }
+  # before    => Class['mysql'],
+}
+
+mysql::db { 'mydb':
+    user     => 'myuser',
+    password => 'mypass',
+    host     => 'localhost',
+    grant    => ['all'],
+    require  => Class['mysql::server'],
+}
+
+database_user {'billy@localhost':
+  ensure        => present,
+  password_hash => mysql_password('billy'),
+  require       => Class['mysql::server'],
+}
+
+
+
+/*
+class { 'mysql': 
+  # before => Class['new_user'],
+}
+
+class new_user {
+  mysql::db { 'mydb':
+    user     => 'myuser',
+    password => 'mypass',
+    host     => 'localhost',
+    grant    => ['all'],
+  }
+}
+
+class { 'new_user': 
+  require => Class['mysql::server'],
+
+}
+*/
